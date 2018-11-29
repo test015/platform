823187ed8ad34ecbd4aa976089599b17cdbbdbfe
Michael Desa <mjdesa@gmail.com>
2018-11-29 12:45:02 -0800

    feat(bolt): add boltdb implementation of kv.Store

---
+++ bolt/kv.go
@@ -0,0 +1,175 @@
+package bolt
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	bolt "github.com/coreos/bbolt"
+	"github.com/influxdata/platform/kv"
+	"go.uber.org/zap"
+)
+
+type KVStore struct {
+	path   string
+	db     *bolt.DB
+	logger *zap.Logger
+}
+
+func NewKVStore(path string) *KVStore {
+	return &KVStore{
+		path:   path,
+		logger: zap.NewNop(),
+	}
+}
+
+// Open / create boltDB file.
+func (s *KVStore) Open(ctx context.Context) error {
+	// Ensure the required directory structure exists.
+	if err := os.MkdirAll(filepath.Dir(s.path), 0700); err != nil {
+		return fmt.Errorf("unable to create directory %s: %v", s.path, err)
+	}
+
+	if _, err := os.Stat(s.path); err != nil && !os.IsNotExist(err) {
+		return err
+	}
+
+	// Open database file.
+	db, err := bolt.Open(s.path, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return fmt.Errorf(ErrUnableToOpen, err)
+	}
+	s.db = db
+
+	s.logger.Info("Resources opened", zap.String("path", s.path))
+	return nil
+}
+
+// Close the connection to the bolt database
+func (s *KVStore) Close() error {
+	if s.db != nil {
+		return s.db.Close()
+	}
+	return nil
+}
+
+func (s *KVStore) WithLogger(l *zap.Logger) {
+	s.logger = l
+}
+
+func (s *KVStore) WithDB(db *bolt.DB) {
+	s.db = db
+}
+
+func (s *KVStore) View(fn func(tx kv.Tx) error) error {
+	return s.db.View(func(tx *bolt.Tx) error {
+		return fn(&Tx{tx})
+	})
+}
+
+func (s *KVStore) Update(fn func(tx kv.Tx) error) error {
+	return s.db.Update(func(tx *bolt.Tx) error {
+		return fn(&Tx{tx})
+	})
+}
+
+func (s *KVStore) Tx(writable bool) (kv.Tx, error) {
+	tx, err := s.db.Begin(writable)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Tx{
+		tx: tx,
+	}, nil
+
+}
+
+type Tx struct {
+	tx *bolt.Tx
+}
+
+func (tx *Tx) CreateBucketIfNotExists(b []byte) error {
+	_, err := tx.tx.CreateBucketIfNotExists(b)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tx *Tx) Bucket(b []byte) (kv.Bucket, error) {
+	return &Bucket{
+		bucket: tx.tx.Bucket(b),
+	}, nil
+}
+
+func (tx *Tx) Commit() error {
+	return tx.tx.Commit()
+}
+
+func (tx *Tx) Rollback() error {
+	return tx.tx.Rollback()
+}
+
+type Bucket struct {
+	bucket *bolt.Bucket
+}
+
+func (b *Bucket) Get(key []byte) ([]byte, error) {
+	val := b.bucket.Get(key)
+	if len(val) == 0 {
+		return nil, kv.ErrKeyNotFound
+	}
+
+	return val, nil
+}
+
+func (b *Bucket) Put(key []byte, value []byte) error {
+	return b.bucket.Put(key, value)
+}
+
+func (b *Bucket) Delete(key []byte) error {
+	return b.bucket.Delete(key)
+}
+
+func (b *Bucket) Cursor() (kv.Cursor, error) {
+	return &Cursor{
+		cursor: b.bucket.Cursor(),
+	}, nil
+}
+
+type Cursor struct {
+	cursor *bolt.Cursor
+}
+
+func (c *Cursor) Seek(prefix []byte) ([]byte, []byte, error) {
+	k, v := c.cursor.Seek(prefix)
+	// TODO(desa): what possible errors?
+	return k, v, nil
+}
+
+func (c *Cursor) First() ([]byte, []byte, error) {
+	k, v := c.cursor.First()
+	// TODO(desa): what possible errors?
+	return k, v, nil
+}
+
+func (c *Cursor) Last() ([]byte, []byte, error) {
+	k, v := c.cursor.Last()
+	// TODO(desa): what possible errors?
+	return k, v, nil
+}
+
+func (c *Cursor) Next() ([]byte, []byte, error) {
+	k, v := c.cursor.Next()
+	// TODO(desa): what possible errors?
+	return k, v, nil
+}
+
+func (c *Cursor) Prev() ([]byte, []byte, error) {
+	k, v := c.cursor.Prev()
+	// TODO(desa): what possible errors?
+	return k, v, nil
+}
---
+++ bolt/kv_test.go
@@ -0,0 +1,82 @@
+package bolt_test
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/influxdata/platform/bolt"
+	"github.com/influxdata/platform/kv"
+)
+
+func NewTestKVStore() (*bolt.KVStore, func(), error) {
+	f, err := ioutil.TempFile("", "influxdata-platform-bolt-")
+	if err != nil {
+		return nil, nil, errors.New("unable to open temporary boltdb file")
+	}
+	f.Close()
+
+	path := f.Name()
+	s := bolt.NewKVStore(path)
+	if err := s.Open(context.TODO()); err != nil {
+		return nil, nil, err
+	}
+
+	close := func() {
+		s.Close()
+		os.Remove(path)
+	}
+
+	return s, close, nil
+}
+
+// TODO: more tests here for the bolt kv store
+
+func TestKVStore(t *testing.T) {
+	s, close, err := NewTestKVStore()
+	if err != nil {
+		t.Fatalf("failed to create test kv store: %v", err)
+	}
+	defer close()
+
+	tx, err := s.Tx(true)
+	if err != nil {
+		t.Fatalf("error creating transaction: %v", err)
+	}
+	defer tx.Rollback()
+
+	if err := tx.CreateBucketIfNotExists([]byte("example")); err != nil {
+		t.Fatalf("error creating bucket: %v", err)
+	}
+
+	b, err := tx.Bucket([]byte("example"))
+	if err != nil {
+		t.Fatalf("error getting bucket: %v", err)
+	}
+
+	if err := b.Put([]byte("hello"), []byte("world")); err != nil {
+		t.Fatalf("error putting kv: %v", err)
+	}
+
+	val, err := b.Get([]byte("hello"))
+	if err != nil {
+		t.Fatalf("error getting value: %v", err)
+	}
+
+	if !bytes.Equal(val, []byte("world")) {
+		t.Fatalf("error getting value. got %v, expected world", string(val))
+	}
+
+	other, err := b.Get([]byte("world"))
+	if err != kv.ErrKeyNotFound {
+		t.Fatalf("expected key not found error got %v", err)
+	}
+
+	if other != nil {
+		t.Fatalf("expected other to be nil got %v", string(other))
+	}
+
+}
