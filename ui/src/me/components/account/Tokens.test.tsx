2d91bdff780de6008cd64ae3fdf3870b4bdc3184
Andrew Watkins <watts@influxdb.com>
2018-12-10 14:44:59 -0800

    test(tokens): update tests

--- ui/src/me/components/account/Tokens.test.tsx
+++ ui/src/me/components/account/Tokens.test.tsx
@@ -14,29 +14,39 @@ const setup = (override?) => {
     ...override,
   }
 
-  const wrapper = mount(<Tokens {...props} />)
+  const tokensWrapper = mount(<Tokens {...props} />)
 
-  return {wrapper}
+  return {tokensWrapper}
 }
 
 describe('Account', () => {
+  let wrapper
+  beforeEach(done => {
+    const {tokensWrapper} = setup()
+    wrapper = tokensWrapper
+    process.nextTick(() => {
+      wrapper.update()
+      done()
+    })
+  })
+
   describe('rendering', () => {
     it('renders!', () => {
-      const {wrapper} = setup()
-
       expect(wrapper.exists()).toBe(true)
       expect(wrapper).toMatchSnapshot()
     })
 
-    it('displays the list of tokens', done => {
-      const {wrapper} = setup()
-
-      process.nextTick(() => {
-        wrapper.update()
-        const rows = wrapper.find(TokenRow)
-        expect(rows.length).toBe(2)
-        done()
-      })
+    it('displays the list of tokens', () => {
+      const rows = wrapper.find(TokenRow)
+      expect(rows.length).toBe(2)
     })
   })
+
+  // describe('user interaction', () => {
+  //   describe('clicking the token description', () => {
+  //     it('opens the ViewTokenModal', () => {
+  //       const {wrapper} = setup()
+  //     })
+  //   })
+  // })
 })
