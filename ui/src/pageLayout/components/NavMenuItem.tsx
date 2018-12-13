90d8033b8efee9cfa9468b7dc1cfab934efc950e
Alex P <thealexpaxton@gmail.com>
2018-12-13 15:07:23 -0800

    Prevent nav menu items from rendering child count as submenu

--- ui/src/pageLayout/components/NavMenuItem.tsx
+++ ui/src/pageLayout/components/NavMenuItem.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, {SFC} from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router'
 import classnames from 'classnames'
 import _ from 'lodash'
@@ -20,32 +20,39 @@ interface Props {
   highlightWhen: string[]
 }
 
-const NavMenuItem: SFC<Props> = ({
-  icon,
-  title,
-  link,
-  children,
-  location,
-  highlightWhen,
-}) => {
-  const {length} = _.intersection(_.split(location, '/'), highlightWhen)
-  const isActive = !!length
+class NavMenuItem extends Component<Props> {
+  public render() {
+    const {icon, title, link} = this.props
 
-  return (
-    <div className={classnames('nav--item', {active: isActive})}>
-      <Link className="nav--item-icon" to={link}>
-        <span className={`icon sidebar--icon ${icon}`} />
-      </Link>
-      <div className="nav--item-menu">
-        <Link className="nav--item-header" to={link}>
-          {title}
+    return (
+      <div className={classnames('nav--item', {active: this.isActive})}>
+        <Link className="nav--item-icon" to={link}>
+          <span className={`icon sidebar--icon ${icon}`} />
         </Link>
-        {React.Children.count(children) && (
-          <Select type={NavMenuSubItem}>{children}</Select>
-        )}
+        <div className="nav--item-menu">
+          <Link className="nav--item-header" to={link}>
+            {title}
+          </Link>
+          {this.children}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  private get isActive(): boolean {
+    const {location, highlightWhen} = this.props
+    const {length} = _.intersection(_.split(location, '/'), highlightWhen)
+
+    return !!length
+  }
+
+  private get children(): JSX.Element | JSX.Element[] {
+    const {children} = this.props
+
+    if (React.Children.count(children)) {
+      return <Select type={NavMenuSubItem}>{children}</Select>
+    }
+  }
 }
 
 export default NavMenuItem
